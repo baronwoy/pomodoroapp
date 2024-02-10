5fef6dda35ec78d1934fff740da7e24ce527f17a
baronwoy <woybaron@gmail.com>
2024-02-10 17:43:37 +0000

    implement  functions to pause, tick seconds down and switch mode

--- src/Timer.js
+++ src/Timer.js
@@ -3,7 +3,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
-import { useContext } from 'react';
+import { useContext, useState, useEffect, useRef } from 'react';
 import SettingsContext from './SettingsContext';
 const red = '#E51C1C'
 
@@ -11,6 +11,53 @@ const red = '#E51C1C'
 
 function Timer() {
     const settingsInfo = useContext(SettingsContext)
+
+    const [isPaused, setIsPaused] = useState(false)
+    const [mode, setMode] = useState('Work'); // work/break/null
+    const [secondsLeft, setSecondsLeft] = useState(0)
+
+    const secondsLeftRef = useRef(secondsLeft);
+    const isPausedRef = useRef(isPaused);
+    const modeRef = useRef(mode);
+
+
+    function switchMode() {
+        const nextMode = modeRef.current === 'work' ? 'break' : 'work';
+        const nextSeconds = (nextMode === 'work' ? settingsInfo.workMinutes : settingsInfo.breakMinutes)*60;
+        setMode(nextMode);
+        modeRef.current = nextMode
+        setSecondsLeft(nextSeconds)
+
+        secondsLeftRef.current = nextSeconds;
+    }
+
+    function tick() {
+        secondsLeftRef.current--;
+        setSecondsLeft(secondsLeftRef.current);
+    }
+
+    function initTimer() {
+        setSecondsLeft(settingsInfo.workMinutes * 60)
+
+    }
+
+    useEffect(() => {
+        initTimer();
+
+        const interval = setInterval(() => {
+            if (isPausedRef.current) {
+                return;
+            }
+            if (secondsLeftRef.current === 0) {
+                return switchMode();
+            }
+
+            tick();
+        }, 1000)
+
+        return clearInterval(interval);
+    },  [settingsInfo]);
+
     return (
         <div>
             <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
@@ -20,9 +67,10 @@ function Timer() {
             })} />
 
             <div style={{marginTop: '20px;'}}>
-                <PlayButton />
-                <PauseButton />
 
+                {isPaused ? <PlayButton /> : <PauseButton /> }
+                
+               
             </div>
             <div style={{marginTop: '20px'}}>
                 <SettingsButton onClick= {() => settingsInfo.setShowSettings(true)}/>
