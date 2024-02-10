1f1fcc33b3e21445075b11fffffbedaf0c8b590b
baronwoy <woybaron@gmail.com>
2024-02-10 13:09:06 +0000

    Import Playbutton and add css for it

--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,6 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton';
 
 const red = '#E51C1C'
 const green = '#1CE51C'
@@ -10,9 +11,10 @@ function Timer() {
             <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
                 textColor: '#fff',
                 pathColor: red,
-                tailColor: '#BEBEBE' 
-            })} />;
-            <div>
+                trailColor: '#333'
+            })} />
+            <div style={{marginTop: '20px;'}}>
+                <PlayButton />
 
             </div>
 
